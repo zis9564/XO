99e12102fb017c9ae8f30afe65f2419ae0ef5864
zis9564 <zis9564@gmail.com>
2020-02-29 18:05:57 +0300

    Player created

---
+++ test/zis/model/PlayerTest.java
@@ -0,0 +1,16 @@
+package zis.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getFigure() {
+    }
+}
