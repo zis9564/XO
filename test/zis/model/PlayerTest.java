97d0a4b953c2e84c7b6abadfecc3896b3a167e24
zis9564 <zis9564@gmail.com>
2020-02-29 18:26:35 +0300

    PlayerTest created

--- test/zis/model/PlayerTest.java
+++ test/zis/model/PlayerTest.java
@@ -8,9 +8,19 @@ public class PlayerTest {
 
     @Test
     public void getName() {
+
+        final String expectedValue = "Ivan";
+        final Player player = new Player(expectedValue, null);
+        final String actualValue = player.getName();
+        assertEquals(expectedValue, actualValue);
     }
 
     @Test
     public void getFigure() {
+
+        final Figure expectedValue = Figure.X;
+        final Player player = new Player(null, expectedValue);
+        final Figure actualValue = player.getFigure();
+        assertEquals(expectedValue, actualValue);
     }
 }
